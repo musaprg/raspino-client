4d1a5fb86208bb75424524ce781e03ca16a1807f
musaprg <musaprg@users.noreply.github.com>
2017-12-24 14:20:29 +0900

    add requirements.txt

--- src/client.py
+++ src/client.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import subprocess
+
 def main():
-    print("Hello World")
+    subprocess.run("echo \"Hello World\"", shell=True, check=True)
 
 if __name__ == '__main__':
     main()
