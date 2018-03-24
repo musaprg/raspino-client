b7f393b8b8a7bb07d99e6e965639a8655721db4a
musaprg <musaprg@users.noreply.github.com>
2018-03-24 10:39:25 +0900

    Configure serial connection

--- src/client.py
+++ src/client.py
@@ -8,12 +8,12 @@ def main():
     ser = serial.Serial(
     port = "/dev/ttyUSB0",
     baudrate = 115200,
-    parity = serial.PARITY_NONE,
-    bytesize = serial.EIGHTBITS,
-    stopbits = serial.STOPBITS_ONE,
-    timeout = None,
-    xonxoff = 0,
-    rtscts = 0,
+    #parity = serial.PARITY_NONE,
+    #bytesize = serial.EIGHTBITS,
+    #stopbits = serial.STOPBITS_ONE,
+    #timeout = None,
+    #xonxoff = 0,
+    #rtscts = 0,
     )
 
     ser.write(b"1145141919810")
